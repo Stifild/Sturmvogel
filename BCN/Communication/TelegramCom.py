a593f4c55654bdd9d31401db63ff3fb1e78dd6a1
Kirill <fedorovkirillwww@yandex.ru>
2025-04-30 15:25:08 +0300

    init

---
+++ BCN/Communication/TelegramCom.py
@@ -0,0 +1,30 @@
+from BCN.Proccesing.utils import Utils as IOP
+import telebot
+from BCN.twm import TellWithMe as TWM
+from telebot import util, types
+
+io = IOP()
+config = io.load_json("env.json")
+twm = TWM()
+MAIN_USER_ID = config["MAIN_USER_ID"]
+bot = telebot.TeleBot(config["BOT_TOKEN"])
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    if message.from_user.id == MAIN_USER_ID:
+        bot.send_message(message.chat.id, 'Start command do nothing, just say hello')
+    else:
+        bot.send_message(message.chat.id, 'You are not authorized to use this bot.')
+
+@bot.message_handler(commands=["send"])
+def sendCommand(message: types.Message):
+    if message.from_user.id == MAIN_USER_ID:
+        rawCommand = util.extract_arguments(message.text)
+        computerName = rawCommand.split("/")[0]
+        command = rawCommand.split("")[1]
+        for address, data in twm.twmcd.items():
+            if data["name"] == computerName:
+                twm.send(twm.compile_command("IC", {"command": command}), address, "IC", f"Listen {data['name']}! I want to you do {command}")
+                bot.send_message(message.chat.id, f"Command {command} sent to {data['name']}")
+    else:
+        bot.send_message(message.chat.id, 'You are not authorized to use this bot.')
---
+++ run.py
@@ -0,0 +1,27 @@
+from BCN.Proccesing.utils import Utils as IOP
+from BCN.twm import TellWithMe as SeTWM
+from Subord.twm import TellWithMe as SuTWM
+import os, socket, platform, time
+
+io = IOP()
+
+configs = io.load_json("./data/env.json")
+
+if configs["role"] == "Server":
+    from BCN.Communication.TelegramCom import bot
+    twm = SeTWM()
+    while True:
+        if twm.check_for_messages():
+            receive, flag = twm.receive()
+            bot.send_message(configs["Telegram"]["MAIN_USER_ID"], f"Flag:{flag}, Receive:{str(receive)}") if flag == "IR" else None
+        time.sleep(3.5)
+        
+         
+else:
+    twm = SuTWM()
+    twm.send(twm.compile_command("IND", f"{socket.gethostname()}, {platform.platform()}"), "0.0.0", "IND", "Hi Server! Can you give me an address?")
+    while True:
+        if twm.check_for_messages():
+            receive, flag = twm.receive()
+            os.system(str(receive)) if flag == "IC" else None
+        time.sleep(3.5)
